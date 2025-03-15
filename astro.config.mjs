f36bb74d0b394ea4ed1ace8bf87ad281dba00a4c
rkris17 <rkris17@gmail.com>
2025-03-15 04:50:18 +0000

    docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
+  output: 'static',
   integrations: [mdx(), sitemap()],
 
   vite: {
